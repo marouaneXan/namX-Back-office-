4b22dd0ac95c366c75fa6c5cf4667eb19377c60f
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-05 22:57:37 +0100

    add TypeContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import {Login, Resgister, Dashboard ,Cars} from './pages/index'
-import Layaout from './components/common/Layaout'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login, Resgister, Dashboard, Cars } from "./pages/index";
+import Layaout from "./components/common/Layaout";
+import TypeContextProvider from "./Context/Car/Types";
 function App() {
-
   return (
     <Router>
-      <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/register' element={<Resgister />} />
-      <Route path='/dashboard' element={<Layaout> {<Dashboard />} </Layaout>} />
-      <Route path='Cars' element={<Layaout> {<Cars />} </Layaout>} />
-      </Routes>
+      <TypeContextProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Resgister />} />
+          <Route
+            path="/dashboard"
+            element={<Layaout> {<Dashboard />} </Layaout>}
+          />
+          <Route path="Cars" element={<Layaout> {<Cars />} </Layaout>} />
+        </Routes>
+      </TypeContextProvider>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
