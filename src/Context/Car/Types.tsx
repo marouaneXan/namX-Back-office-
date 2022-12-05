ed0fd6a47fdeaac9f280bdece0aac623a1ef26df
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-05 16:16:27 +0100

    getAllTypes and adding Proxy

--- src/Context/Car/Types.tsx
+++ src/Context/Car/Types.tsx
@@ -1,17 +1,43 @@
-import { createContext, useState, useMemo } from "react";
+import { createContext, useState, useMemo, useEffect } from "react";
+import axios from "axios"
 export const TypeContext = createContext(null);
 const TypeContextProvider = ({ children }: any) => {
   const [types, setTypes] = useState();
-
+  const [loading,setLoading]=useState<Boolean>(false)
+  const [empty, setEmpty] = useState<boolean>(false);
   // get all types
   const getTypes = async () => {
-    
+    setLoading(true);
+    const res = await axios.get(`${Proxy}/types`).catch((err) => {
+      const message: any =
+        (err.res && err.res.data && err.res.data.message) || err || err.message;
+      if (message) {
+        setLoading(false);
+        setEmpty(message.response.data.message);
+        setTimeout(() => {
+          setLoading(false);
+        }, 4000);
+      }
+    });
+    if (res && res.data) {
+      setLoading(false);
+      setTypes(res.data);
+    }
   };
+  useEffect(()=>{
+    getTypes()
+  },[])
   
   const values: any = useMemo(
     () => ({
+      types,
+      empty,
+      loading
     }),
     [
+      types,
+      empty,
+      loading
     ]
   );
   return <TypeContext.Provider value={values}>{children}</TypeContext.Provider>;
