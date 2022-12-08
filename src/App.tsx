62f56b21cecd3741f24cc4bf68b9e02a81df5b12
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-08 11:00:06 +0100

    unstall some packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-
-import Layaout from "./components/common/Layaout";
 import TypeContextProvider from "./Context/Car/Types";
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import {Login, Resgister, Dashboard ,Cars, Details} from './pages/index'
-import Types from "./components/Type/Types";
+import {Login, Resgister} from './pages/index'
 function App() {
   return (
     <Router>
@@ -12,13 +9,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Resgister />} />
-          <Route
-            path="/dashboard"
-            element={<Layaout> {<Dashboard />} </Layaout>}
-          />
-          <Route path="cars" element={<Layaout> {<Cars />} </Layaout>} />
-          <Route path="details" element={<Layaout> {<Details />} </Layaout>} />
-          <Route path="types" element={<Layaout> {<Types />} </Layaout>} />
         </Routes>
       </TypeContextProvider>
     </Router>
