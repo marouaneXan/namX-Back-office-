5033b796275cdd989f871f1d4b6f719f3fe73aef
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-10 23:40:54 +0100

    add Type route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Sidebar from "./components/Layouts/Sidebar";
 import Navbar from "./components/Layouts/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Cars from "./pages/Cars";
+import Types from "./pages/Types";
 function App() {
   return (
     <Router>
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/cars" element={<Cars />} />
+          <Route path="/types" element={<Types />} />
         </Routes>
       </TypeContextProvider>
     </Router>
