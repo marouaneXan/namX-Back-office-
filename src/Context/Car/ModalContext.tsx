4aaf95f423921eb51a1039ece041eb3b651f4cdf
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-11 14:35:22 +0100

    create show function

--- src/Context/Car/ModalContext.tsx
+++ src/Context/Car/ModalContext.tsx
@@ -2,12 +2,20 @@ import { createContext, useState, useMemo } from "react";
 export const ModalContext = createContext(null);
 const ModalContextProvider = ({ children }: any) => {
   const [open, setOpen] = useState<Boolean>(false);
+
+  const show=()=>{
+    setOpen(true)
+  }
   
   
   const values: any = useMemo(
     () => ({
+        open,
+        show
     }),
     [
+        open,
+        show
     ]
   );
   return <ModalContext.Provider value={values}>{children}</ModalContext.Provider>;
