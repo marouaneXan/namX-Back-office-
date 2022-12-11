98baf1185bdcf89f72d8072ef867a916308d5b5c
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-11 14:33:47 +0100

    create open state

--- src/Context/Car/ModalContext.tsx
+++ src/Context/Car/ModalContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState, useMemo } from "react";
 export const ModalContext = createContext(null);
 const ModalContextProvider = ({ children }: any) => {
+  const [open, setOpen] = useState<Boolean>(false);
   
   
   const values: any = useMemo(
