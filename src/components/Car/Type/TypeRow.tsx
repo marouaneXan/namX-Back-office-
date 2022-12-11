d59dd3f544eb258e44752241423c5f2c6b3d8805
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-11 15:51:02 +0100

    show modal event

--- src/components/Car/Type/TypeRow.tsx
+++ src/components/Car/Type/TypeRow.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ModalContext } from "../../../Context/Car/ModalContext";
 import { Color, Type } from "../../../types/CarTypes";
 interface Props {
   type: Type;
 }
 
 const TypeRow = (props: Props) => {
+  const { show }: any = useContext(ModalContext);
   return (
     <tr className="hover:bg-gray-100">
       <td className="p-4 text-sm font-normal text-gray-500 whitespace-nowrap lg:p-5">
@@ -27,7 +29,10 @@ const TypeRow = (props: Props) => {
         </div>
       </td>
       <td className="p-4 flex items-center mt-1 space-x-1 whitespace-nowrap lg:p-5">
-        <button className="inline-flex items-center px-2 py-2 text-gray-800 text-sm font-medium rounded-md">
+        <button
+          onClick={show}
+          className="inline-flex items-center px-2 py-2 text-gray-800 text-sm font-medium rounded-md"
+        >
           View
         </button>
         <button className="inline-flex items-center px-2 py-2 text-red-500 text-sm font-medium rounded-md">
