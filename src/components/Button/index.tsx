11a04f3d1830ea20605ccc573908a93509fee9af
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-12 11:42:49 +0100

    create first version for modal add new type

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ModalContext } from "../../Context/Car/ModalContext";
 
 interface ButtonProps{
     content:string
 }
 
 const Button = (props:ButtonProps) => {
+    const {show}:any=useContext(ModalContext)
   return (
     <button
+    onClick={show}
       className="my-1 w-[130px] text-white bg-emerald-500 bg-gradient-to-r from-[#5651e5] to-[#709dff] font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:scale-[1.02] transition-transform"
     >
       {props.content}
--- src/pages/Types.tsx
+++ src/pages/Types.tsx
@@ -1,16 +1,25 @@
 import React, { useContext } from "react";
 import Button from "../components/Button";
+import Add from "../components/Car/Type/Add";
 import TypeRow from "../components/Car/Type/TypeRow";
+import { ModalContext } from "../Context/Car/ModalContext";
 import { TypeContext } from "../Context/Car/Types";
 import { Type } from "../types/CarTypes";
 
 const Types = () => {
   const { types }: any = useContext(TypeContext);
+  const { open }: any = useContext(ModalContext);
   return (
     <div className="flex overflow-hidden bg-white pt-16">
       <div className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
         <div className="flex flex-col my-6 mx-4 rounded-2xl shadow-xl shadow-gray-200">
-          <Button content="Add new Type"/>
+          <Button content="Add new Type" />
+          {open && (
+            <>
+              <Add />
+              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            </>
+          )}
           <div className="overflow-x-auto rounded-2xl">
             <div className="inline-block min-w-full align-middle">
               <div className="overflow-hidden">
